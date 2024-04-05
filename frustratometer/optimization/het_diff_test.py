1302db7dc4e3b7d7dc814259c1fe7227d75d1ec8
MengyanWang <mw88@rice.edu>
2024-04-05 14:23:02 -0500

    hete_diff more simplified

--- frustratometer/optimization/het_diff_test.py
+++ frustratometer/optimization/het_diff_test.py
@@ -31,17 +31,17 @@ def heterogeneity(sequence):
     return np.log(het)
     # return het
 
-def np(seq):
-    N = len(seq)
-    amino_acid_counts = Counter(seq)
-    np_numerator = factorial(N)
-    np_denominator = 1
-    for count in amino_acid_counts.values():
-        np_denominator *= factorial(count)
-    return np_numerator / np_denominator
+# def np(seq):
+#     N = len(seq)
+#     amino_acid_counts = Counter(seq)
+#     np_numerator = factorial(N)
+#     np_denominator = 1
+#     for count in amino_acid_counts.values():
+#         np_denominator *= factorial(count)
+#     return np_numerator / np_denominator
 
 def het_diff_simplify(mutated_sequence):
-    aa_old = "S"
+    aa_old = "I"
     aa_new = "A"
     amino_acid_counts = Counter(mutated_sequence)
     n_old = amino_acid_counts.get(aa_old, 0) + 1  
@@ -50,8 +50,19 @@ def het_diff_simplify(mutated_sequence):
 
     return het_diff
 
-sequence = list("SISSRVKSKRIQLGLNQAELAQKVGTTQQSIEQLENGKTKRPRFLPELASALGVSVDWLLNGT")
-mutated_sequence = list("AISSRVKSKRIQLGLNQAELAQKVGTTQQSIEQLENGKTKRPRFLPELASALGVSVDWLLNGT")#mut the first aa from S to A
+def het_diff_simplify2(mutated_sequence):
+    aa_old = "I"
+    aa_new = "A"
+    amino_acid_counts = Counter(mutated_sequence)
+    n_old = amino_acid_counts.get(aa_old, 0) + 1  
+    n_new = amino_acid_counts[aa_new] - 1 
+    # het_diff = log(factorial(n_old) * factorial(n_new) / (factorial(n_old - 1) * factorial(n_new + 1)))
+    het_diff = log(n_old /(n_new+1) )
+
+    return het_diff
+
+sequence = list("SISSRVKSKRIQLGLNQAELAQKVGTTQQ")
+mutated_sequence = list("SASSRVKSKRIQLGLNQAELAQKVGTTQQ")#mut the first aa from S to A
 # het_diff =  log(heterogeneity(mutated_sequence)) -  log(heterogeneity(sequence))
 het_diff =  heterogeneity(mutated_sequence) - heterogeneity(sequence)
 # het_diff = log(np(mutated_sequence)/np(sequence))
@@ -59,6 +70,7 @@ print('Before Hetergeneity Simplification', het_diff)
 # mutated_sequence, het_diff = sequence_mutation(sequence)
 het_diff_simplify = het_diff_simplify(mutated_sequence=mutated_sequence)
 print('After Hetergeneity Simplification', het_diff_simplify)
+assert np.allclose(het_diff,het_diff_simplify)
 
 
     
